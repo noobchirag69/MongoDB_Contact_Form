88b4b0c8fbdcf9620cc6693c8ddf9b753911bf95
Chirag Chakraborty <chiragchakraborty48@gmail.com>
2022-09-20 12:01:58 +0530

    Done

--- index.js
+++ index.js
@@ -6,15 +6,12 @@ const Message = require('./models/message');
 // Initiating Express
 const app = express();
 
-// Configuring Dotenv
-require('dotenv').config({ path: "./.env" });
-
 // Connect to MongoDB
-const dbURI = process.env.MONGODB_URI;
+const dbURI = "mongodb+srv://chiragiemlabs007:zhGacYs39r019JPw@notepadcluster.tnx3oxu.mongodb.net/?retryWrites=true&w=majority";
 
 // Starting the Server based on Database Connectivity
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => app.listen(process.env.PORT))
+    .then(result => app.listen(process.env.PORT || 3000))
     .catch(err => console.log(err));
 
 // Middlewares
